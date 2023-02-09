089740276fb0601e44304954dd36415cdd04cd40
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-09 22:35:15 +0300

    add Example 005 Condition If-Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") // приведение строки к Lower Case. Обратите внимание, что сравнимаем с Lower Case
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
